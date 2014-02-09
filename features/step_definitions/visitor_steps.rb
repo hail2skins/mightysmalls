64d116e1878b9a4d8b574ea747b5d8feb3001c62
Art Mills <art@hamcois.com>
2014-02-09 18:51:36 +0000

    First feature update

--- features/step_definitions/visitor_steps.rb
+++ features/step_definitions/visitor_steps.rb
@@ -1,11 +1,23 @@
-Given(/^I am at the home page of the site as a guest$/) do
+Given(/^I visit "(.*?)" as a visitor$/) do |arg1|
   visit '/'
 end
 
-Given(/^I can see the site is in alpha testing$/) do
+Given(/^I can see the site is in "(.*?)" testing$/) do |arg1|
   page.should have_content("ALPHA")
 end
 
-Given(/^I can see a link to "Sign up now!"$/) do
-  page.should have_link("Sign up now!")
+Given(/^I can see a link to "(.*?)"$/) do |arg1|
+  page.should have_content("Sign up now!")
+end
+
+Given(/^I can see a header with the "(.*?)" name$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I can see a footer with "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I can see navigation links for help, about, contact and sign in$/) do
+  pending # express the regexp above with the code you wish you had
 end
--- features/visitor_visiting.feature
+++ features/visitor_visiting.feature
@@ -1,11 +1,14 @@
 Feature: Visitor Visiting
-	In order to see the home page of the Hamco Internet Solutions CMS page
+	In order to see the home page at "http://www.mightysmalls.com"
 	As a visitor who has not signed up
 	I should see a default landing page with basic information about the site
 	that allows me to sign up.
 
 	Scenario: Visiting the home page
-		Given I am at the home page of the site as a guest
-		And I can see the site is in alpha testing
+		Given I visit "http://www.mightysmalls.com" as a visitor
+		And I can see the site is in "ALPHA" testing
 		And I can see a link to "Sign up now!"
+		And I can see a header with the "Mighty Smalls" name
+		And I can see a footer with "Hamco Internet Solutions"
+		And I can see navigation links for help, about, contact and sign in
 	
