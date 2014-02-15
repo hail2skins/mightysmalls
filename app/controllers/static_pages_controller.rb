24a75f60ab67bc48d1883c5cc1fb4f5a0a399756
Art Mills <art@hamcois.com>
2014-02-15 05:53:13 +0000

    Redirect for logged in user.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
 	def home
+		if owner_signed_in?
+			redirect_to current owner
+		else
+			render 'static_pages/home'
+		end
 	end
 
 	def about
