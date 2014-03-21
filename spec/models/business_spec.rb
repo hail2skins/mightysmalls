23a99ef17dc46635737b792ed13e59f577a5ef82
Art Mills <art@hamcois.com>
2014-03-20 23:16:58 -0500

    Testing soft delete for business.

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -14,5 +14,40 @@
 require 'spec_helper'
 
 describe Business do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:owner) { FactoryGirl.create(:owner) }
+	let(:business) {FactoryGirl.create(:business, owner: owner) }
+  
+  it "retains business and updates deleted_at column when destroy called" do
+  	expect(business.deleted_at).to be(nil)
+    business.destroy
+    business.deleted_at.should_not be(nil)
+  end
+
+  it "restore sets deleted_at to nil" do
+  	business.destroy
+  	business.deleted_at.should_not be(nil)
+  	business.restore
+  	business.deleted_at.should be(nil)
+  end
+
+  it "deletes business for real using really_destroy!" do
+  	business.deleted_at.should be(nil)
+  	expect { business.really_destroy! }.to change(Business, :count).from(1).to(0)
+  end
+
+
+	it "should have a #name" do
+ 		business.should respond_to(:name)
+ 	end
+
+ 	it "should have a #description" do
+ 		business.should respond_to(:description)
+ 	end
+
+ 	it "should require #name" do
+   	no_name_business = FactoryGirl.build(:business, owner: owner, name: "")
+   	no_name_business.should_not be_valid
+  end
+
+  
 end
